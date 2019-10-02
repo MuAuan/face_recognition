73442c358440e2ccc3e6fba0718ba5d7abf77561
MuAuan <35788955+MuAuan@users.noreply.github.com>
2019-10-02 21:49:27 +0900

    Add files via upload

--- recognize_vgg16_camera.py
+++ recognize_vgg16_camera.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+from pykakasi import kakasi
+
 import cv2
 import numpy as np
 from time import sleep
@@ -17,17 +20,19 @@ def cv_fourcc(c1, c2, c3, c4):
             ((ord(c3) & 255) << 16) + ((ord(c4) & 255) << 24)
 
 def yomikomi(model,img):
-    name_list=['まゆゆ','さっしー','きたりえ','‎じゅりな','おぎゆか','あかりんだーすー']
-    x = image.img_to_array(img)
+    name_list=['まゆゆ','あかりんだーすー','ウワン'] #['まゆゆ','さっしー','きたりえ','‎じゅりな','おぎゆか','あかりんだーすー']
+    x = img/255 #image.img_to_array(img)
+    cv2.imshow("x",x)
     x = np.expand_dims(x, axis=0)
     preds = model.predict(x)
-    index=np.argmax(preds)
-    print(index,name_list[index])
-    return name_list[index]
+    index=np.argmax(preds[0])
+    print(preds[0])
+    print(index,name_list[index],preds[0][index])
+    return name_list[index], preds[0][index]
 
 def model_definition():
     batch_size = 2
-    num_classes = 6
+    num_classes = 3
     img_rows, img_cols=224,224
     input_tensor = Input((img_rows, img_cols, 3))
     # 学習済みのVGG16をロード
@@ -44,15 +49,23 @@ def model_definition():
     # VGG16とFCを接続
     model = Model(input=vgg16.input, output=top_model(vgg16.output))
     model.summary()
-    model.load_weights('params_model_VGG16L3_i_100.hdf5')
+    model.load_weights('history_params_model_VGG16L3_i_100.hdf5')
     return model
 
 def main():
+    kakasi_ = kakasi()
+    kakasi_.setMode('H', 'a')
+    kakasi_.setMode('K', 'a')
+    kakasi_.setMode('J', 'a')
+    conv = kakasi_.getConverter()
+    
     #カスケード分類器の特徴量を取得する
     cascade = cv2.CascadeClassifier(cascade_path)
     color = (255, 255, 255) #白
+    path = "./img/"
+    label = str(input("人を判別する数字を入力してください ex.0："))
 
-    OUT_FILE_NAME = "./outputs/face_recognition.avi"
+    OUT_FILE_NAME = "./img/face_recognition.avi"
     FRAME_RATE=1
     w=224 #1280
     h=224 #960
@@ -71,6 +84,7 @@ def main():
     fontpath ='C:\Windows\Fonts\HGRPP1.TTC' # Windows10 だと C:\Windows\Fonts\ 以下にフォントがあります。
     font = ImageFont.truetype(fontpath, 16) # フォントサイズが32
     font0 = cv2.FONT_HERSHEY_SIMPLEX
+    sk=0
     while True:
         b,g,r,a = 0,255,0,0 #B(青)・G(緑)・R(赤)・A(透明度)
         timer = cv2.getTickCount()
@@ -88,28 +102,30 @@ def main():
         if len(facerect) > 0:
             #検出した顔を囲む矩形の作成
             for rect in facerect:
-                y,x=tuple(rect[0:2])
-                y2,x2=tuple(rect[0:2]+rect[2:4])
-                print(y,x,y2,x2)
-                cv2.rectangle(frame, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
-                roi = frame[y:y2, x:x2]  #frame[y:y+h, x:x+w]
+                x=rect[0]
+                y=rect[1]
+                width=rect[2]
+                height=rect[3]
+                roi = img[y:y+height, x:x+width]  #frame[y:y+h, x:x+w]
+                cv2.rectangle(img, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
+                
                 try:
-                    #cv2.imshow('roi0',roi)
                     roi = cv2.resize(roi, (int(224), 224))
                     cv2.imshow('roi',roi)
-                    txt=yomikomi(model,roi)
-                    print("txt",txt)
+                    txt, preds=yomikomi(model,roi)
+                    print("txt, preds",txt,preds*100 ," %")
+                    txt2=conv.do(txt)
+                    cv2.imwrite(path+"/"+label+"/"+str(sk)+'_'+str(txt2)+'_'+str(int(preds*100))+'.jpg', roi)
+                    img_pil = Image.fromarray(img) # 配列の各値を8bit(1byte)整数型(0～255)をPIL Imageに変換。
+                    draw = ImageDraw.Draw(img_pil) # drawインスタンスを生成
+                    position = (x, y) # テキスト表示位置
+                    draw.text(position, txt, font = font , fill = (b, g, r, a) ) # drawにテキストを記載 fill:色 BGRA (RGB)
+                    img = np.array(img_pil) # PIL を配列に変換
                 except:
                     txt=""
                     continue
-                img_pil = Image.fromarray(frame) # 配列の各値を8bit(1byte)整数型(0～255)をPIL Imageに変換。
-                draw = ImageDraw.Draw(img_pil) # drawインスタンスを生成
-                position = (y, x) # テキスト表示位置
-                draw.text(position, txt, font = font , fill = (b, g, r, a) ) # drawにテキストを記載 fill:色 BGRA (RGB)
-                img = np.array(img_pil) # PIL を配列に変換
-                #cv2.putText(frame, txt, (y,x), font = font0, 0.75, (150,170,150), 2);
         cv2.imshow('test',img)
-        #cv2.imshow('test',frame)
+        sk +=1
         key = cv2.waitKey(1)&0xff
         
         if is_video=="True":
